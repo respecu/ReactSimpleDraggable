0201dfaee169ece5225e9b604b43a23a877482fb
norte13 <norte13@imform.co.kr>
2021-01-15 11:45:14 +0900

    declare type of function

--- src/components/Draggable.tsx
+++ src/components/Draggable.tsx
@@ -8,7 +8,7 @@ interface DraggableState {
 }
 
 export default class Draggable extends Component<any, DraggableState> {
-    private _draggable: any = createRef();
+    private _draggable = createRef<HTMLDivElement>().current;
     
     constructor(props: any) {
         super(props);
@@ -20,33 +20,52 @@ export default class Draggable extends Component<any, DraggableState> {
         }
     }
 
-    componentDidUpdate(props: any, state: any) {
-        if (this.state.dragging && !state.dragging) {
-          document.addEventListener('mousemove', this.onMouseMove)
-          document.addEventListener('mouseup', this.onMouseUp)
-        } else if (!this.state.dragging && state.dragging) {
-          document.removeEventListener('mousemove', this.onMouseMove)
-          document.removeEventListener('mouseup', this.onMouseUp)
-        }
+    componentDidMount() {
+        document.addEventListener('mousemove', this.onMouseMove)
+        document.addEventListener('mouseup', this.onMouseUp)
     }
 
-    onMouseMove = (e: any) => {
+    componentWillUnmount() {
+        document.removeEventListener('mousemove', this.onMouseMove)
+        document.removeEventListener('mouseup', this.onMouseUp)
+    }
+
+    shouldComponentUpdate(props: any, state: DraggableState) {
+        const { top, left, dragging } = this.state;
+        if(top !== state.top || left !== state.left || dragging !== this.state.dragging ) {
+            return true;
+        }
+
+        return false;
+    }
+
+    // componentDidUpdate(props: any, state: DraggableState) {
+    //     if (this.state.dragging && !state.dragging) {
+    //       document.addEventListener('mousemove', this.onMouseMove)
+    //       document.addEventListener('mouseup', this.onMouseUp)
+    //     } else if (!this.state.dragging && state.dragging) {
+    //       document.removeEventListener('mousemove', this.onMouseMove)
+    //       document.removeEventListener('mouseup', this.onMouseUp)
+    //     }
+    // }
+
+    onMouseMove = (e: MouseEvent) => {
         if (!this.state.dragging) {
             return;
         }
 
-        let left = e.x - this._draggable.offsetWidth / 2;
-        let top = e.y - this._draggable.offsetHeight / 2;
+        let left = e.x - this._draggable!.offsetWidth / 2;
+        let top = e.y - this._draggable!.offsetHeight / 2;
 
         if(left > window.innerWidth) {
-            left = window.innerWidth - this._draggable.clientWidth;
+            left = window.innerWidth - this._draggable!.clientWidth;
         }
         else if(left < 0) {
             left = 0;
         }
 
         if(top > window.innerHeight) {
-            top = window.innerHeight - this._draggable.clientHeight;
+            top = window.innerHeight - this._draggable!.clientHeight;
         }
         else if(top < 0) {
             top = 0;
@@ -64,19 +83,19 @@ export default class Draggable extends Component<any, DraggableState> {
         e.preventDefault();
       }
     
-      onMouseUp = (e: any) => {
+      onMouseUp = (e: MouseEvent) => {
         this.setState({dragging: false});
         e.stopPropagation();
         e.preventDefault();
       }
     
-      onMouseDown = (e: any) => {
+      onMouseDown = (e: React.MouseEvent<Element, MouseEvent>) => {
         if (e.button !== 0) {
             return;
         }
 
-        const left = e.clientX - this._draggable.offsetWidth / 2;
-        const top = e.clientY - this._draggable.offsetHeight / 2;
+        const left = e.clientX - this._draggable!.offsetWidth / 2;
+        const top = e.clientY - this._draggable!.offsetHeight / 2;
 
         // console.log(left, top);
 
